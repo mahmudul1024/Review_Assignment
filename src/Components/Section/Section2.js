c05e1b845d49e789e95563187e0616db58d0dac5
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-12-04 16:59:57 +0100

    slider perfection done and privateroute issues with login data solved

--- src/Components/Section/Section2.js
+++ src/Components/Section/Section2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import im1 from "../../assets/images/banner/1.jpg";
 import im2 from "../../assets/images/banner/2.jpg";
 import im3 from "../../assets/images/banner/3.jpg";
@@ -8,6 +8,15 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
 
 const Section2 = () => {
+  const [oldreview, setOldreview] = useState([]);
+  useEffect(() => {
+    fetch("https://review-server-three.vercel.app/oldReview")
+      .then((res) => res.json())
+      .then((data) => setOldreview(data));
+  }, []);
+
+  console.log(oldreview);
+
   return (
     <Splide
       options={{
@@ -27,93 +36,42 @@ const Section2 = () => {
       }}
       extensions={{ AutoScroll }}
     >
-      <SplideSlide>
-        <div class="bg-white dark:bg-gray-800 w-72 shadow-lg mx-auto rounded-xl p-4">
-          <p class="text-gray-600 dark:text-white">
-            <span class="text-lg font-bold text-indigo-500">“</span>
-            To get social media testimonials like these, keep your customers
-            engaged with your social media accounts by posting regularly
-            yourself
-            <span class="text-lg font-bold text-indigo-500">”</span>
-          </p>
-          <div class="flex items-center mt-4">
-            <a href="#" class="relative block">
-              <img
-                alt="profil"
-                src={im1}
-                class="mx-auto object-cover rounded-full h-10 w-10 "
-              />
-            </a>
-            <div class="flex flex-col justify-between ml-2">
-              <span class="text-sm font-semibold text-indigo-500">
-                Jean Miguel
-              </span>
-              <span class="flex items-center text-xs dark:text-gray-400">
-                User of Tail-Kit
-                <img src={""} class="w-4 h-4 ml-2" />
-              </span>
+      {oldreview.map((old) => {
+        return (
+          <SplideSlide>
+            <div class="bg-slate-200  w-80 shadow-lg mx-auto rounded-xl p-10 ">
+              <p class="text-gray-600 text-white ">
+                <span class="  text-2xl  font-bold text-indigo-500">
+                  {old.topic}
+                </span>
+              </p>
+              <p class="text-gray-600 text-white">
+                <span class="text-lg font-bold text-indigo-500">“</span>
+                {old.areatext}
+                <span class="text-lg font-bold text-indigo-500">”</span>
+              </p>
+              <div class="flex items-center mt-4">
+                <a href="#" class="relative block">
+                  <img
+                    alt="profil"
+                    src={old.photo}
+                    class="mx-auto object-cover rounded-full h-10 w-10 "
+                  />
+                </a>
+                <div class="flex flex-col justify-between ml-2">
+                  <span class="text-sm font-semibold text-indigo-500">
+                    {old.name}
+                  </span>
+                  <span class="flex items-center text-xs dark:text-gray-400">
+                    {old.email}
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </SplideSlide>
-      <SplideSlide>
-        <div class="bg-white dark:bg-gray-800 w-72 shadow-lg mx-auto rounded-xl p-4">
-          <p class="text-gray-600 dark:text-white">
-            <span class="text-lg font-bold text-indigo-500">“</span>
-            To get social media testimonials like these, keep your customers
-            engaged with your social media accounts by posting regularly
-            yourself
-            <span class="text-lg font-bold text-indigo-500">”</span>
-          </p>
-          <div class="flex items-center mt-4">
-            <a href="#" class="relative block">
-              <img
-                alt="profil"
-                src={im2}
-                class="mx-auto object-cover rounded-full h-10 w-10 "
-              />
-            </a>
-            <div class="flex flex-col justify-between ml-2">
-              <span class="text-sm font-semibold text-indigo-500">
-                Jean Miguel
-              </span>
-              <span class="flex items-center text-xs dark:text-gray-400">
-                User of Tail-Kit
-                <img src={""} class="w-4 h-4 ml-2" />
-              </span>
-            </div>
-          </div>
-        </div>
-      </SplideSlide>
-      <SplideSlide>
-        <div class="bg-white dark:bg-gray-800 w-72 shadow-lg mx-auto rounded-xl p-4">
-          <p class="text-gray-600 dark:text-white">
-            <span class="text-lg font-bold text-indigo-500">“</span>
-            To get social media testimonials like these, keep your customers
-            engaged with your social media accounts by posting regularly
-            yourself
-            <span class="text-lg font-bold text-indigo-500">”</span>
-          </p>
-          <div class="flex items-center mt-4">
-            <a href="#" class="relative block">
-              <img
-                alt="profil"
-                src={im3}
-                class="mx-auto object-cover rounded-full h-10 w-10 "
-              />
-            </a>
-            <div class="flex flex-col justify-between ml-2">
-              <span class="text-sm font-semibold text-indigo-500">
-                Jean Miguel
-              </span>
-              <span class="flex items-center text-xs dark:text-gray-400">
-                User of Tail-Kit ki
-                <img src={""} class="w-4 h-4 ml-2" />
-              </span>
-            </div>
-          </div>
-        </div>
-      </SplideSlide>
+          </SplideSlide>
+        );
+      })}
+
       <div className="splide__arrows">
         <button className="splide__arrow splide__arrow--prev">Prev</button>
         <button className="splide__arrow splide__arrow--next">Next</button>
